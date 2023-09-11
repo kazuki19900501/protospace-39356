4125aff96b69bd0468f7376bf539036711ec1766
kazuki19900501 <fkazuki19900501@gmail.com>
2023-09-11 19:04:39 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  #devise_for :users
   root to: "prototypes#index"
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: :create 
+    resources :comments, only: :create
   end
   resources :users, only: :show
+  #resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    #resources :comments, only: :create 
+  #end
+  #resources :users, only: :show
 end
